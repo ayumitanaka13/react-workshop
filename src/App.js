34783c8d15ab3ab9b270066511c1722ffe11ed76
ayumitanaka <tanakayumi13@gmail.com>
2020-12-11 13:02:25 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+        <Clock date={new Date()} />
+        {/*<Clock />*/}
       </header>
     </div>
   );
