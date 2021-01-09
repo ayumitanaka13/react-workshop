a4ca8c023e5d02483a108b3ec1c4fba08bcf22ec
ayumitanaka <tanakayumi13@gmail.com>
2021-01-08 16:08:33 -0800

    modified ClockWithUse.js

--- src/ClockWithUse.js
+++ src/ClockWithUse.js
@@ -5,7 +5,8 @@ import media from "styled-media-query"
 
 const Text = styled.p`
 font-size: 40px;
-color: lime;
+color: ${(props) => props.color};
+
 ${media.greaterThan("large")`
   font-size: 20px;
   color: deepskyblue;
@@ -15,26 +16,29 @@ ${media.greaterThan("large")`
 const ClockWithUse = props => {
     //constractor [getter, setter] = default
     const [count, setCount] = useState(0)
+    const [fontColor, setFontColor] = useState('deepskyblue')
     //function
     const countUp = () => setCount(count + 1)
+    const updateFontColor = () => setFontColor('red')
 
     useEffect(() => {
       console.log('%cuseEffect', 'color:skyblue; font-size: 20px;')
+      if (count >= 10) {
+        updateFontColor();
+      }
     }, [count])
 
     return (
         <React.Fragment>
-        <h1>Hello, world!</h1>
-        <h2>It is {props.date.toLocaleTimeString()}.</h2>
-        {/* function */}
-        <button onClick={countUp}>Count UP</button>
-        {/* getter */}
-        <Text>Count {count}</Text>
+          <h1>Hello, world!</h1>
+          <h2>It is {props.date.toLocaleTimeString()}.</h2>
+          {/* function */}
+          <button onClick={countUp}>Count UP</button>
+          {/* getter */}
+          <Text color={fontColor}>Count {count}</Text>
         </React.Fragment>
     )
 }
-
-
 
 export default ClockWithUse
 
