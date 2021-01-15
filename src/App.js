fa59fd6943e01d7e28e51793ac8696333e11b2f3
ayumitanaka <tanakayumi13@gmail.com>
2021-01-15 13:39:19 -0800

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-import ClockWithUse from './ClockWithUse';
-import Clock from './Clock';
+// routing setting
+import React from 'react'
+// need {} except for export default name
+import { Reset } from 'styled-reset'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
+import Header from './components/Header'
 
+import Index from './pages/Index'
+import ProductDetail from './pages/ProductDetail'
+import RegistProduct from './pages/RegistProduct'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Reset />
+      <Router>
+        <Switch>
+          <Route path='/header'>
+            <Header />
+          </Route>
+          <Route path='/product-detail'>
+            <ProductDetail />
+          </Route>
+          <Route path='/regist-product'>
+            <RegistProduct />
+          </Route>
+          {/* path='/' should be bottom */}
+          <Route path='/'>
+            <Index />
+          </Route>
+        </Switch>
+      </Router>
+    </React.Fragment>
+  )
+}
+
+export default App;
+
+// Old Code
+
+// import logo from './logo.svg';
+// import './App.css';
+// import ClockWithUse from './ClockWithUse';
+// import Clock from './Clock';
+
+{/*
 function App() {
   return (
     <div className="App">
@@ -21,10 +66,11 @@ function App() {
           Learn React
         </a>
         <ClockWithUse date={new Date()} />
-        {/* <Clock date={new Date()}/> */}
+        <Clock date={new Date()}/>
       </header>
     </div>
   );
 }
 
 export default App;
+ */}
