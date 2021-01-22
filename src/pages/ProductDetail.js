1b9609db560a59856e63908601a2bbbd2039d9f1
ayumitanaka <tanakayumi13@gmail.com>
2021-01-22 13:00:01 -0800

    added Wrapper.js

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Wrapper from '../components/Wrapper'
 
 const ProductDetail = props => {
   return (
     <React.Fragment>
-      <h1>ProductDetail</h1>
-      <p><Link to='/'>Top</Link></p>
-      <p><Link to='/regist-product'>to regist-product</Link></p>
+      <Wrapper>
+        <h1>Product Detail</h1>
+      </Wrapper>
     </React.Fragment>
   )
 }
